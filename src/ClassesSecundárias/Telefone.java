32a38fb58c52a3d4ade92ea83039ff9c7d55fa6c
Alexandre <alexandrewn0402@gmail.com>
2025-04-15 21:10:59 -0300

    Telefone Corrigido

--- src/ClassesSecundárias/Telefone.java
+++ src/ClassesSecundárias/Telefone.java
@@ -5,20 +5,34 @@ import java.util.Scanner;
 public class Telefone {
 
     private String numero;
+    private boolean chamadaAtiva = false;
     Scanner scanner = new Scanner(System.in);
-    
+
     public void ligar() {
         System.out.println("Digite o número do telefone: ");
         numero = scanner.nextLine();
-        scanner.close();
-        System.out.println("Ligando para " + numero + "...");
+        if (numero == null || numero.isEmpty()) {
+            System.out.println("Número inválido.");
+        } else {
+            System.out.println("Ligando para " + numero + "...");
+            chamadaAtiva = true; 
+        }
     }
 
     public void atender() {
-        System.out.println("Atendendo chamada...");
+        if (chamadaAtiva) {
+            System.out.println("Atendendo chamada de " + numero + "...");
+            System.out.println("Chamada em andamento.");
+        } else {
+            System.out.println("Nenhuma chamada para atender.");
+        }
     }
-    
+
     public void iniciarCorreioVoz() {
-        System.out.println("Iniciando correio de voz...");
+        if (!chamadaAtiva) {
+            System.out.println("Nenhuma chamada ativa. Gravando mensagem no correio de voz...");
+        } else {
+            System.out.println("Não é possível iniciar o correio de voz enquanto há uma chamada ativa.");
+        }
     }
 }
